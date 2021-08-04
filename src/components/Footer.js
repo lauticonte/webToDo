42a40bca5873d566027e678824f67f9e6662b309
Lautaro Conte <lauty.conte@gmail.com>
2021-08-04 11:28:28 -0300

    Cambios footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,6 @@ const Footer = () => {
   return (
     <footer>
       <p>
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge">
-          Frontend Mentor
-        </a>
-        .
-      </p>
-      <p>
         Coded by <a href="http://contelautaro.com.ar" className="footer-class">Lautaro Conte</a>.
       </p>
     </footer>
